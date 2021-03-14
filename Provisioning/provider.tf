74fbc975006ce1044b2208c5eef6c108872d0e39
peppe2794 <39173356+peppe2794@users.noreply.github.com>
2021-03-14 20:16:37 +0100

    Add files via upload

--- Provisioning/provider.tf
+++ Provisioning/provider.tf
@@ -7,6 +7,6 @@ terraform {
   }
 }
 provider "proxmox" {
-    pm_api_url = "https://{IP_PROXMOX}/api2/json"
+    pm_api_url = "https://10.224.16.41:8006/api2/json"
     pm_tls_insecure = "true"
 }
--- Provisioning/resource.tf
+++ Provisioning/resource.tf
@@ -1,41 +1,42 @@
 
 variable "ip_list"{
   type  =  list
-  default  =  ["{IP1}","{IP2}"]
+  default  =  ["192.168.6.76","192.168.6.97"]
 }
 variable "credentials_ciuser"{
   type  =  list
-  default  =  ["{USER1}","{USER2}"]
+  default  =  ["master","worker"]
 }
 variable "credentials_cipassword"{
   type  =  list
-  default  =  ["{PASSWORD1}","{PASSWORD2}"]
+  default  =  ["master","worker"]
 }
 
 resource "proxmox_vm_qemu" "proxmox_vm1" {
-  count             = {VM_COUNT}
-  name              = "{VM_NAME}${count.index}"
+  count             = 2
+  name              = "kub${count.index}"
   target_node       = "pve"
-  clone             = "{VM_TEMPLATE_NAME}"
+  clone             = "Deploy"
   desc              = <<-EOT
             user: ${var.credentials_ciuser[count.index]}
             password: ${var.credentials_cipassword[count.index]}
             root pass: ${var.credentials_cipassword[count.index]}
         EOT
-  ciuser=  ${var.credentials_ciuser[count.index]}
-  cipassword=  ${var.credentials_cipassword[count.index]}
-  memory = {MEMORY_IN_MB}
-  cores = "{CORE}"
+  ciuser =  var.credentials_ciuser[count.index]
+  cipassword =  var.credentials_cipassword[count.index]
+
+  memory = 8192
+  cores = "4"
   cpu = "kvm64"
-  pool = "{POOL}"
+  pool = "Tesi_Zagaria"
   define_connection_info = false
   hastate    =    "started"
   
   #Example  ipconfig0  = "ip=192.168.6.1${count.index+1}/24,gw=192.168.6.1" 
-  ipconfig0  = "ip=${var.ip_list[count.index]}/24,gw={GATEWAY}"
+  ipconfig0  = "ip=${var.ip_list[count.index]}/24,gw=192.168.6.1"
   
 disk {
-  size         = "{SIZE_IN_MB}"
+  size         = "10000M"
   storage      = "nas_storage"
   type         = "scsi"
 }
